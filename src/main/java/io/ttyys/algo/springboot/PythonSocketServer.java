faa72ca0c7b9ddd7fc6c8ecb7e5c5f4926a140ff
teng wang <wangteng0829@gmail.com>
2021-03-15 19:57:19 +0800

    tengwang: 1. add annotation-processor 2.add start python server runner

--- src/main/java/io/ttyys/algo/springboot/PythonSocketServer.java
+++ src/main/java/io/ttyys/algo/springboot/PythonSocketServer.java
@@ -3,16 +3,26 @@ package io.ttyys.algo.springboot;
 import org.apache.commons.exec.CommandLine;
 import org.apache.commons.exec.DefaultExecutor;
 import org.apache.commons.exec.Executor;
+import org.apache.commons.io.FileUtils;
 import org.springframework.util.ResourceUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
 
 public class PythonSocketServer {
+
+    private static final String tempPath = System.getProperty("java.io.tmpdir");
+
+
     public void start() throws IOException {
-        File execution = ResourceUtils.getFile("bin/PythonSocketServer");
-        CommandLine cmdLine = new CommandLine(execution.getAbsolutePath());
+        InputStream stream = getClass().getClassLoader().getResourceAsStream("bin/server");
+        File targetFile = new File(tempPath + "PythonSocketServer");
+        FileUtils.copyInputStreamToFile(stream, targetFile);
         Executor executor = new DefaultExecutor();
+        executor.execute(CommandLine.parse("chmod +x " + targetFile.getAbsolutePath()));
+        CommandLine cmdLine = new CommandLine(targetFile.getAbsolutePath());
         executor.execute(cmdLine);
     }
 }
